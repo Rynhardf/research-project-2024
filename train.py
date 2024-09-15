ca8ee5b90fb027b6b4902ccb3a1fd4598720b24c
Rynhardf <rynhard@diefouries.com>
2024-09-15 12:13:07 +0200

    revert

--- train.py
+++ train.py
@@ -14,6 +14,31 @@ import argparse
 from utils.utils import save_config, get_model
 
 
+def load_config(config_path):
+    with open(config_path, "r") as file:
+        config = yaml.safe_load(file)
+    return config
+
+
+def save_config(config, save_path):
+    with open(save_path, "w") as file:
+        yaml.dump(config, file)
+
+
+def get_model(config):
+    if config["name"] == "HRNet":
+        from models.HRNet.hrnet import PoseHighResolutionNet
+
+        model = PoseHighResolutionNet(config["config"])
+        if config["weights"]:
+            model.init_weights(config["weights"])
+    else:
+        raise ValueError(
+            f"Model {config['model']['name']} not recognized"
+        )  # Added error handling
+    return model
+
+
 def get_optimizer(config, model):
     optimizer_name = config["training"]["optimizer"].lower()
     if optimizer_name == "adam":
